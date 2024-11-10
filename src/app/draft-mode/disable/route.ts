4703852802b783cb14337d047fa3b564af342a2a
stirwin <stirwincastro@gmail.com>
2024-11-09 23:56:51 -0500

    full debug

--- src/app/draft-mode/disable/route.ts
+++ src/app/draft-mode/disable/route.ts
@@ -1,7 +1,7 @@
 import { draftMode } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextResponse){
+export async function GET(request: NextRequest){
     await(await draftMode()).disable();
     return NextResponse.redirect(new URL("/", request.url));
     
